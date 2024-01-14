99502c2dfd062793751ce61634278dfffeeee2d0
Benjamin Martinis <martinisbenjamin@gmail.com>
2024-01-14 17:48:07 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## HippoExchange 
+
+HippoExchange is a online marketplace to buy and sell digital goods. I have built this useing express, nextjs, react, and tailwind. 
+
+Live website: 
---
+++ src/hooks/use-cart.ts
@@ -0,0 +1,34 @@
+// add items
+// remove items
+// clear cart
+
+import { Product } from "@/payload-types";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+export type CartItems = {
+    product: Product
+};
+
+type CartState = {
+    items: CartItems[],
+    addItem: (product: Product) => void,
+    removeItem: (productID: string) => void,
+    clearCart: () => void,
+};
+
+export const useCart = create<CartState>()(
+    persist((set) => ({
+        items: [],
+        addItem: (product) => set((state) => {
+            return { items: [...state.items, { product }] }
+        }),
+        removeItem: (id) => set((state) => ({
+            items: state.items.filter((item) => item.product.id !== id)
+        })),
+        clearCart: () => set({ items: [] })
+    }), {
+        name: "cart-storage",
+        storage: createJSONStorage(() => localStorage),
+    })
+);
