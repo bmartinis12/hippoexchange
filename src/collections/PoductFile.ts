fff41a5c860a7a48261b6f9aaf610cc5e285e041
Benjamin Martinis <martinisbenjamin@gmail.com>
2024-01-16 19:07:30 -0600

    Update database permissions

--- src/collections/PoductFile.ts
+++ src/collections/PoductFile.ts
@@ -3,54 +3,65 @@ import { BeforeChangeHook } from "payload/dist/collections/config/types";
 import { Access, CollectionConfig } from "payload/types";
 
 const addUser: BeforeChangeHook = ({ req, data }) => {
-    const user = req.user as User | null;
+    const user = req.user as User | null
     return { ...data, user: user?.id }
-};
-
-
+}
 
 const yourOwnAndPurchased: Access = async ({ req }) => {
-    const user = req.user as User | null;
+    const user = req.user as User | null
 
-    if (user?.role === 'admin') return true;
-    if (!user) return false;
+    if (user?.role === 'admin') return true
+    if (!user) return false
 
     const { docs: products } = await req.payload.find({
-        collection: "products",
+        collection: 'products',
         depth: 0,
         where: {
-            users: {
-                equals: user.id
-            }
-        }
-    });
+            user: {
+                equals: user.id,
+            },
+        },
+    })
 
-    const ownProductFileIds = products.map((prod) => prod.product_files).flat();;
+    const ownProductFileIds = products
+        .map((prod) => prod.product_files)
+        .flat()
 
     const { docs: orders } = await req.payload.find({
-        collection: "orders",
+        collection: 'orders',
         depth: 2,
         where: {
-            users: {
-                equals: user.id
-            }
-        }
-    });
+            user: {
+                equals: user.id,
+            },
+        },
+    })
 
-    const purchasedProductFileIds = orders.map((order) => {
-        return order.products.map((product) => {
-            if (typeof product == "string") return req.payload.logger.error("Search depth not sufficient to find purchased file IDs");
+    const purchasedProductFileIds = orders
+        .map((order) => {
+            return order.products.map((product) => {
+                if (typeof product === 'string')
+                    return req.payload.logger.error(
+                        'Search depth not sufficient to find purchased file IDs'
+                    )
 
-            return typeof product.product_files === 'string' ? product.product_files : product.product_files.id;
+                return typeof product.product_files === 'string'
+                    ? product.product_files
+                    : product.product_files.id
+            })
         })
-    }).filter(Boolean).flat();
+        .filter(Boolean)
+        .flat()
 
     return {
         id: {
-            in: [...ownProductFileIds, ...purchasedProductFileIds]
-        }
-    };
-};
+            in: [
+                ...ownProductFileIds,
+                ...purchasedProductFileIds,
+            ],
+        },
+    }
+}
 
 export const ProductFiles: CollectionConfig = {
     slug: 'product_files',
@@ -66,20 +77,24 @@ export const ProductFiles: CollectionConfig = {
         delete: ({ req }) => req.user.role === 'admin',
     },
     upload: {
-        staticURL: "/product_files",
-        staticDir: "product_files",
-        mimeTypes: ["image/*", "font/*", "application/postscript"],
+        staticURL: '/product_files',
+        staticDir: 'product_files',
+        mimeTypes: [
+            'image/*',
+            'font/*',
+            'application/postscript',
+        ],
     },
     fields: [
         {
-            name: "user",
-            type: "relationship",
-            relationTo: "users",
+            name: 'user',
+            type: 'relationship',
+            relationTo: 'users',
             admin: {
                 condition: () => false,
             },
             hasMany: false,
             required: true,
-        }
-    ]
+        },
+    ],
 }
